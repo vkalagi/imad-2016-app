9fa45c8065751808eb80d3a54f3e7a298333234c
vkalagi <habeebnee@gmail.com>
2016-10-18 11:43:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var articleOne ={
                  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.This is the content for my first article. This is the content for my first article. This is the content for my first article.
             </p>`
     };
-
+    
 function createtemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -55,15 +55,15 @@ var htmltemplate = `
 </html>
      `;  
     return htmltemplate;     `
-    }
-    
+}
+  
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
  res.send(createTemplate(articleOne));
-    });
+ });
     
 app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
@@ -80,6 +80,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+}
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
