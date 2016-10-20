ffbb9bd43bc1c821e4901964692489b702898346
vkalagi <habeebnee@gmail.com>
2016-10-20 17:29:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,15 +87,15 @@ app.get('/:articleName', function (req, res) {
  res.send(createTemplate(articles[articleName]));
  });
     
-app.get(`/ui/style.css`, function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', `style.css`));
+app.get('/ui/style.css', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get(`/ui/main.js`, function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', `main.js`));
+app.get('/ui/main.js', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get(`/ui/madi.png`, function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', `madi.png`));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
