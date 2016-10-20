32fd32a8bb03297bcaea006aacec1f419b53dfae
vkalagi <habeebnee@gmail.com>
2016-10-20 14:38:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,9 @@ app.get('/:articleName', function (req, res) {
  res.send(createTemplate(articles[articleName]));
  });
     
-
+app.get('/ui/style.css', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
