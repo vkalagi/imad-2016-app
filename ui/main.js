0c85d397efcad32e9747554fbbaf210c5bf8395a
vkalagi <habeebnee@gmail.com>
2016-10-21 18:31:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+// Move the image
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
