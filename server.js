2484ceaf182c448fb251ad7427e21d91334b9b2a
vkalagi <habeebnee@gmail.com>
2016-10-22 12:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "ui", 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req,res)
+    {counter = counter + 1;
+    req.send(counter.toString());
+    });
 app.get('/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one
