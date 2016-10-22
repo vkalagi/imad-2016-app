e30fcb50d5d575253c17f0dbe27be5c111d82aeb
vkalagi <habeebnee@gmail.com>
2016-10-22 18:03:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ app.get('/counter', function (req,res)
     {counter = counter + 1;
     res.send(counter.toString());
     });
+    
 app.get('/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one
@@ -98,6 +99,7 @@ app.get('/:articleName', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
      res.sendFile(path.join(__dirname, "ui", 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
      res.sendFile(path.join(__dirname, "ui", 'main.js'));
 });
@@ -105,7 +107,6 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, "ui", 'madi.png'));
 });
-
 
 var names = [];
 app.get ('/submit-name/:name', function(req, res){
