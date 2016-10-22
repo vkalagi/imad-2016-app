f38f18d25d064d6a0bd829fcd0a88fe8dad2c99a
vkalagi <habeebnee@gmail.com>
2016-10-22 18:54:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,14 +29,32 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
-    // Make a request to the server an send the name
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200) {
+             // Make a request to the server an send the name
     
     // Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for (var i=0; i< names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+        }
+    }
+        // Not yet done
+    };
+ 
+    // Make the request
+    request.open('GET', 'http://vkalagi.imad.hasura-app.io/submit-name?name=' +name, true);
+    request.send(null); 
+    
 };
