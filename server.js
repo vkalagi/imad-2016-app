7c5427c9c4ed8ddb6ab563a1b4ae08d4b17b7d5d
vkalagi <habeebnee@gmail.com>
2016-10-22 18:16:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ var articles = {
             </p>`}
 };
     
+    
+    
+    
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -80,14 +83,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "ui", 'index.html'));
 });
 
-
-
-
 var counter = 0;
 app.get('/counter', function (req,res)
     {counter = counter + 1;
     res.send(counter.toString());
     });
+    
+var names = [];
+app.get ('/submit-name', function(req, res){ // URL: /submit-name?name=xxxxx
+    // Get the names from request
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+}); 
     
 app.get('/:articleName', function (req, res) {
     // articleName == article-one
@@ -108,13 +116,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, "ui", 'madi.png'));
 });
 
-var names = [];
-app.get ('/submit-name', function(req, res){ // URL: /submit-name?name=xxxxx
-    // Get the names from request
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 // JSON JavaSript Object Notation
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
